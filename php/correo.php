7f678dea203c7ad366c3c5acdc4a77c8404fce0f
valen <valenbautistao38@.com>
2023-09-12 23:42:21 -0500

    modificacion

--- php/correo.php
+++ php/correo.php
@@ -1,44 +1,37 @@
 <?php
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require('../vendor/autoload.php');
-require('../vendor/phpmailer/phpmailer/src/PHPMailer.php');
-require('../vendor/phpmailer/phpmailer/src/SMTP.php');
-
-if (isset($_POST['email'])) {
-    $para = 'diegoaarenas06@gmail.com';
+if (isset($_POST)) {
+    $para = $_POST['email'];
     $de = 'visionviajera16@gmail.com';
-    try {
-        $name = 'prueba';
-        $asunto = 'Prueba asunto';
-        $msg = 'esta es la prueba';
-        $email = 'visionviajera16@gmail.com';
-        $header = "From: diegoaarenas@gmail.com" . "\r\n";
-        $header .= "Reply-To: visionviajera16@gmail.com" . "\r\n";
-        $header .= "X-Mailer: PHP/" . phpversion();
+    require '../PHPMailer/src/PHPMailer.php';
+    require '../PHPMailer/src/SMTP.php';
+    // Configurar PHPMailer
+    $user = 'visionviajera16@gmail.com';
+    $pass = 'wwxfgrvzgalkccbc';
+    $mailer  = new PHPMailer;
+    $mailer->CharSet = 'UTF-8';
+    $mailer->isSMTP();
+    $mailer->Host = 'smtp.gmail.com';
+    $mailer->SMTPAuth = true;
+    $mailer->Username = $user;
+    $mailer->Password = $pass;
+    $mailer->SMTPSecure = 'tls';
+    $mailer->Port = 587;
+    $mailer->setFrom($user);
+    $mailer->addAddress($para);
+    $mailer->isHTML(true);
 
-        // Configuración de SMTP y conexión segura TLS
-        ini_set("SMTP", "smtp.gmail.com");
-        ini_set("smtp_port", "25");
-        ini_set("sendmail_from", "visionviajera16@gmail.com");
-        ini_set("smtp_ssl", "tls");
+    // Obtener adjuntos
 
-        $mail = @mail($email, $asunto, $msg, $header);
+    // Construir el cuerpo del mensaje
+    $body = '¡Hola ' . $_POST['nombre'] . ' ' . $_POST['apellido'] .  ' recibimos el siguiente mensaje tuyo "' . $_POST['mensaje'] . '" en unos minutos nos contactaremos para despejar tus dudas!';
 
-        if ($mail) {
-            echo 'Se envió correctamente';
-        } else {
-            $error = error_get_last();
-            if ($error) {
-                echo 'Error: ' . $error['message'];
-            } else {
-                echo 'Problemitas';
-            }
-        }
-    } catch (Exception $e) {
-        echo 'Mensaje ' . $mailer->ErrorInfo;
+    $mailer->Subject = 'Correo informativo: ';
+    $mailer->Body = $body;
+
+    // Enviar el correo reenviado
+    if (!$mailer->send()) {
+        echo 'Hubo un error';
+    } else {
+        echo  'Correo enviado exitosamente';
     }
 }
